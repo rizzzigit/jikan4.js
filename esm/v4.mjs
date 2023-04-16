e0139c39c5a55912e4d08d01b86e11f2ab3d680c
Rizzzi Git <rizzzigit@gmail.com>
2023-04-16 13:47:38 +0800

    update exports

--- esm/v4.mjs
+++ esm/v4.mjs
@@ -1,4 +1,4 @@
-export * as v4 from './v4.mjs'
+export * as v1 from './v4/v1.mjs'
 
 export * as default from './v4/v1.mjs'
 export * from './v4/v1.mjs'
---
+++ esm/v4/v1.mjs
@@ -0,0 +1,2 @@
+export * as default from '../../dist/v4/v1'
+export * from '../../dist/v4/v1'

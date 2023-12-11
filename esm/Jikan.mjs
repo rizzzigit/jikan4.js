25c6c6a8a6c480e257aa00e40fe30f5b099ae84d
Rizzzi Git <rizzzigit@gmail.com>
2023-12-11 17:03:48 +0800

    add `next` export

--- esm/Jikan.mjs
+++ esm/Jikan.mjs
@@ -1,4 +1,5 @@
 export * as v4 from './v4.mjs'
+export * as next from './next.mjs'
 
 export * as default from './v4.mjs'
 export * from './v4.mjs'
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * as v4 from './v4'
+export * as next from './next'
 
 export * as default from './v4'
 export * from './v4'

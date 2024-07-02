0ea90055b36156234a9252fc5e3aaa1a0fdb9110
Rizzzi Git <rizzzigit@gmail.com>
2024-07-02 19:18:21 +0800

    update test scripts

--- src/test.ts
+++ src/test.ts
@@ -3,9 +3,18 @@ import { Client } from "./v4";
 type TestFunction<T = unknown> = (cilent: Client) => Promise<T> | T;
 
 const tests: Record<string, TestFunction> = {
-  test: (client) => client.seasons.get('Spring', 2024, {
-    continuing: true
-  }),
+  test: async (client) => {
+    const a = await client.seasons.get('Spring', 2024, {
+      continuing: true
+    })
+    const b = await client.seasons.get('Spring', 2024, {
+      continuing: false
+    })
+    const c = await client.seasons.get('Spring', 2024)
+
+    const result = [a[0], b[0], c[0]]
+    return result.map((anime) => `${anime.title}`)
+  },
 };
 
 const client = new Client();
